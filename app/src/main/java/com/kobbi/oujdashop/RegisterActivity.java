62d8566e7593701aa5a1f941eb17b11f747e0624
abdel-kobbi <mr.abdelkhaliqkobbi@gmail.com>
2025-02-15 17:36:10 +0100

    add register activity & app logo

--- app/src/main/java/com/kobbi/oujdashop/RegisterActivity.java
+++ app/src/main/java/com/kobbi/oujdashop/RegisterActivity.java
@@ -1,9 +1,6 @@
 package com.kobbi.oujdashop;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,31 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
-
-    private EditText email, password;
-    private Button btnLogin, btnNewAccount;
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-
-        btnLogin = findViewById(R.id.btnLogin);
-        btnNewAccount = findViewById(R.id.btnNewAccount);
-
-        btnNewAccount.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-            startActivity(intent);
         });
     }
 }
